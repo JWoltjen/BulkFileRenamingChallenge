39caff157f0a7a8f418173ead35f02d2f31347c0
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-08-15 12:12:34 -0500

    Add project files.

---
+++ BulkFileRenamingChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace RenameFilesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.EnumerateFiles(@"C:\Users\Jeff's PC\source\repos\BulkFileRenamingChallenge\BulkFileRenamingChallenge\PrimaryChallengeFiles");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
