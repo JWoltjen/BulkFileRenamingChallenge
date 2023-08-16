ec76518ad6ef561adf4efe66a0b7b9776708371a
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-08-16 10:44:07 -0500

    Replace all instances of Acme with JeffCo in Title Case.

--- BulkFileRenamingChallenge/Program.cs
+++ BulkFileRenamingChallenge/Program.cs
@@ -8,9 +8,32 @@ namespace RenameFilesApp
         {
             var files = Directory.EnumerateFiles(@"C:\Users\Jeff's PC\source\repos\BulkFileRenamingChallenge\BulkFileRenamingChallenge\PrimaryChallengeFiles");
 
+            // file - \PrimaryChallengeFiles\acme by inc.txt
+            // fileName - acme by acme inc
+            // filePath - \BulkFileRenamingChallenge\PrimaryChallengeFiles
+            // fileExtension - .txt
+
             foreach (var file in files)
             {
-                Console.WriteLine(file);
+                string fileName = Path.GetFileNameWithoutExtension(file);
+                string filePath = Path.GetDirectoryName(file);
+                string fileExtension = Path.GetExtension(file);
+
+                string newFileName = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(fileName.ToLower());
+
+                // by putting spaces around the thing to replace and the thing replacing, it will only replace cases where it is the entire word, not a portion of a random string
+                newFileName = newFileName.Replace(" acme ", " JeffCo ", StringComparison.InvariantCultureIgnoreCase);
+
+                if(newFileName.StartsWith("acme ", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    newFileName = "JeffCo " + newFileName.Substring(5);
+                }
+                if(newFileName.EndsWith(" acme", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    newFileName = newFileName.Substring(0, newFileName.Length - 5) + " JeffCo";
+                }
+
+                Console.WriteLine(newFileName);
             }
         }
     }
